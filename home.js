74a48fc8803ad730604d21e3ddc7704c3379f9ce
Tabrej <dev.tabrej@gmail.com>
2022-09-04 14:15:22 +0530

    initial commit

---
+++ home.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .navbox ul li");
+window.addEventListener("scroll",() => {
+  let current = "";
+// console.log(pageYOffset);
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+   if(scrollY >=sectionTop){
+    current=section.getAttribute('id');
+   }
+})
+
+  navLi.forEach(li => {
+    li.classList.remove('active');
+    if (li.classList.contains(current)) {
+      li.classList.add('active');
+    }
+  })
+})
