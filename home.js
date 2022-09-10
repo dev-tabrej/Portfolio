e097fcb0c26de63a0db5961238b646b997da9acb
Tabrej <dev.tabrej@gmail.com>
2022-09-10 19:42:48 +0530

    responsive and style change

--- home.js
+++ home.js
@@ -2,19 +2,19 @@ const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .navbox ul li");
 window.addEventListener("scroll",() => {
   let current = "";
-// console.log(pageYOffset);
+console.log(scrollY);
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
+    // const sectionHeight = section.clientHeight;
    if(scrollY >=sectionTop){
     current=section.getAttribute('id');
    }
 })
 
   navLi.forEach(li => {
-    li.classList.remove('active');
+    li.classList.remove("active");
     if (li.classList.contains(current)) {
-      li.classList.add('active');
+      li.classList.add("active");
     }
   })
 })
