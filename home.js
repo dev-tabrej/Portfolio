978d3bc41c6a242620afc66874efdbd9cd59459b
dev-tabrej <dev.tabrej@gmail.com>
2022-11-18 17:17:35 +0530

    Style change

--- home.js
+++ home.js
@@ -1,20 +1,23 @@
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav .navbox ul li");
+const sections = document.querySelectorAll(".section");
+// console.log(sections)
+const navLi = document.querySelectorAll("nav .navbox ul.nav_item li");
+// console.log(navLi)
 window.addEventListener("scroll",() => {
   let current = "";
-console.log(scrollY);
+// console.log(scrollY);
   sections.forEach(section => {
     const sectionTop = section.offsetTop;
-    // const sectionHeight = section.clientHeight;
+    const sectionHeight = section.clientHeight;
    if(scrollY >=sectionTop){
-    current=section.getAttribute('id');
+    current=section.getAttribute('Id');
+    console.log(current)
    }
 })
 
   navLi.forEach(li => {
-    li.classList.remove("active");
+    li.classList.remove(".active");
     if (li.classList.contains(current)) {
-      li.classList.add("active");
+      li.classList.add(".active");
     }
   })
 })
