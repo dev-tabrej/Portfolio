974749abf50107908ba17c722a33b6b3414419e6
dev-tabrej <dev.tabrej@gmail.com>
2022-12-03 17:57:00 +0530

    refractoring of code

--- home.js
+++ home.js
@@ -1,23 +1,2 @@
-const sections = document.querySelectorAll(".section");
-// console.log(sections)
-const navLi = document.querySelectorAll("nav .navbox ul.nav_item li");
-// console.log(navLi)
-window.addEventListener("scroll",() => {
-  let current = "";
-// console.log(scrollY);
-  sections.forEach(section => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-   if(scrollY >=sectionTop){
-    current=section.getAttribute('Id');
-    console.log(current)
-   }
-})
-
-  navLi.forEach(li => {
-    li.classList.remove(".active");
-    if (li.classList.contains(current)) {
-      li.classList.add(".active");
-    }
-  })
-})
+const navList = document.querySelectorAll('ul li.list')
+console.log(navList)
